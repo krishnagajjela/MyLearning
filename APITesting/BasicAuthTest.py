8c2f8844d5e267fe82f5a11981e4fafa2e501c43
krishnagajjela <34534667+krishnagajjela@users.noreply.github.com>
2022-07-05 15:01:51 +0530

    Delete BasicAuthTest.py

--- APITesting/BasicAuthTest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import requests
-from requests.auth import HTTPBasicAuth
-
-def test_basiAuth():
-    res = requests.get("https://api.github.com/user", auth=HTTPBasicAuth('krishnagajjela@gmail.com', 'Gkrish@2020'))
-    print(res.text)
-    print("hello1")
